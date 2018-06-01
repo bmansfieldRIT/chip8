ada4e0ab275b3b936d5cddecbfffb7e96bef1a2b
Brian <bmansfieldhfl@gmail.com>
2018-06-01 18:21:24 -0400

    rename init() to initialize()

--- Chip8.h
+++ Chip8.h
@@ -1,12 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Project description
-// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 // Name: myChip8
 //
 // Author: Laurence Muller
 // Contact: laurence.muller@gmail.com
 //
-// License: GNU General Public License (GPL) v2 
+// License: GNU General Public License (GPL) v2
 // ( http://www.gnu.org/licenses/old-licenses/gpl-2.0.html )
 //
 // Copyright (C) 2011 Laurence Muller / www.multigesture.net
@@ -16,29 +16,29 @@ class chip8 {
 	public:
 		chip8();
 		~chip8();
-		
+
 		bool drawFlag;
 
 		void emulateCycle();
 		void debugRender();
-		bool loadApplication(const char * filename);		
+		bool loadApplication(const char * filename);
 
 // Chip8
 		unsigned char  gfx[64 * 32];	// Total amount of pixels: 2048
-		unsigned char  key[16];			
+		unsigned char  key[16];
 
-	private:	
+	private:
 		unsigned short pc;				// Program counter
 		unsigned short opcode;			// Current opcode
 		unsigned short I;				// Index register
 		unsigned short sp;				// Stack pointer
-		
+
 		unsigned char  V[16];			// V-regs (V0-VF)
 		unsigned short stack[16];		// Stack (16 levels)
-		unsigned char  memory[4096];	// Memory (size = 4k)		
-				
-		unsigned char  delay_timer;		// Delay timer
-		unsigned char  sound_timer;		// Sound timer		
+		unsigned char  memory[4096];	// Memory (size = 4k)
 
-		void init();
+		unsigned char  delay_timer;		// Delay timer
+		unsigned char  sound_timer;		// Sound timer
+
+		void initialize();
 };
