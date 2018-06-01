73d1c3de3c06402c177e0fb526c9d36dbf1ae646
Brian <bmansfieldhfl@gmail.com>
2018-06-01 17:23:38 -0400

    remove local variables already declared in class declaration

--- chip8.cpp
+++ chip8.cpp
@@ -12,22 +12,6 @@
 const int SCREEN_WIDTH  = 64;
 const int SCREEN_HEIGHT = 32;
 
-// stores the current opcode
-// opcodes are two bytes (16 bits), so we use a short
-unsigned short opcode;
-
-// 4k memory
-unsigned char memory[4046];
-
-// holds the CPU registers
-// 15 8-bit general purpose registers, 16th is for carry flag
-unsigned char V[16];
-
-// index register 0x00 to 0xFFF
-unsigned short I;
-// program counter 0x000 to 0xFFF
-unsigned short pc;
-
 /*
 * systems memory map:
 * 0x000-0x1FF - Chip8 interpreter, contains font set in emu
@@ -35,22 +19,6 @@ unsigned short pc;
 * 0x200-0XFFF - Program ROM and work RAM
 */
 
-// screen has 64 x 32 = 2048 pixels
-// pixel state is 1 or 0
-unsigned char gfx[SCREEN_WIDTH * SCREEN_HEIGHT];
-
-// countdown timers
-// when set above 0, they will count down at 60Hz
-unsigned char delay_timer;
-// buzzer sounds when sound timer reaches 0
-unsigned char sound_timer;
-
-// 16 level of stack, hold the current location before a jump is performed
-unsigned short stack[16];
-unsigned short sp;
-
-// HEX based keypad 0x0 - 0xF
-unsigned char key[16];
 unsigned char last_key;
 
 // chip8 font set. Each num or char is 4 px wide and 5 px high
