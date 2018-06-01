5c6688471c9c38ba93e602d8da19a4f1cf709cd0
Brian <bmansfieldhfl@gmail.com>
2018-06-01 17:52:48 -0400

    fix assignment error in opcode 8XY6

--- chip8.cpp
+++ chip8.cpp
@@ -239,7 +239,7 @@ void Chip8::emulateCycle(){
                 // 0x8XY6: VX = Vy >> 1. VF set to rightmost VY bit before shift.
                 case 0x0006:{
                     V[15] = (V[(opcode & 0x00F0) >> 4] & 0x0001);
-                    V[(opcode & 0x0F00) >> 8] = V[(opcode 0x00F0) >> 4] >> 1;
+                    V[(opcode & 0x0F00) >> 8] >>= 1;
                     pc += 2;
                     break;
                 }
